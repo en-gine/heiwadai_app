2b4acfcf611fcabd93f222598f9163f5357d207f
tm-sthm <tm.sthm@gmail.com>
2023-07-05 16:45:41 +0900

    ログイン画面　画面構成

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -13,25 +14,46 @@ class LoginScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final formKey = useMemoized(() => GlobalKey<FormState>());
-    final mail = useState(false);
-    final pass = useState(false);
+
+    final isMail = useState(false);
+    final mailError = useState<String?>(null);
+
+    final isPass = useState(false);
+    final passError = useState<String?>(null);
+
+    final isObscure = useState(true);
 
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: MyAppBar(style: AppBarStyle.logo, menu: MenuMode.none),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/wood.png'),
-            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/wood.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              const Text(
-                'login screen',
+              Container(
+                margin: EdgeInsets.only(
+                    top: AppBar().preferredSize.height + 30.w, bottom: 30.w),
+                width: double.infinity,
+                height: 290.w,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'login screen',
+                    ),
+                  ],
+                ),
               ),
               Form(
                 key: formKey,
@@ -47,13 +69,14 @@ class LoginScreen extends HookWidget {
                             RegExp(r'[a-zA-Z0-9@._+-]+'),
                           ),
                         ],
-                        decoration: const InputDecoration(
+                        decoration: InputDecoration(
                           labelText: "メールアドレス",
-                          labelStyle: TextStyle(color: Color(0xFFB3B3B3)),
-                          enabledBorder: OutlineInputBorder(
+                          errorText: mailError.value,
+                          labelStyle: const TextStyle(color: Color(0xFFB3B3B3)),
+                          enabledBorder: const OutlineInputBorder(
                             borderSide: BorderSide(color: Color(0xFFB3B3B3)),
                           ),
-                          focusedBorder: OutlineInputBorder(
+                          focusedBorder: const OutlineInputBorder(
                             borderSide: BorderSide(color: Color(0xFFB3B3B3)),
                           ),
                           filled: true,
@@ -62,8 +85,7 @@ class LoginScreen extends HookWidget {
                         ),
                         autofocus: true,
                         onChanged: (value) {
-                          mail.value = value.isNotEmpty;
-                          print('value: $value, mail: ${mail.value}');
+                          isMail.value = value.isNotEmpty;
                         },
                         onSaved: (value) => () {
                           print('$value');
@@ -71,7 +93,7 @@ class LoginScreen extends HookWidget {
                       ),
                       SizedBox(height: 5.w),
                       TextFormField(
-                        obscureText: true,
+                        obscureText: isObscure.value,
                         keyboardType: TextInputType.visiblePassword,
                         inputFormatters: [
                           FilteringTextInputFormatter.allow(
@@ -80,6 +102,7 @@ class LoginScreen extends HookWidget {
                         ],
                         decoration: InputDecoration(
                           labelText: 'パスワード',
+                          errorText: passError.value,
                           labelStyle: const TextStyle(color: Color(0xFFB3B3B3)),
                           enabledBorder: const OutlineInputBorder(
                             borderSide: BorderSide(color: Color(0xFFB3B3B3)),
@@ -91,26 +114,31 @@ class LoginScreen extends HookWidget {
                           fillColor: Colors.white,
                           isDense: true,
                           suffixIcon: IconButton(
-                            icon: const Icon(Icons.visibility_off),
-                            onPressed: () {},
+                            icon: SvgPicture.asset(
+                              isObscure.value
+                                  ? 'assets/icons/visibility_on.svg'
+                                  : 'assets/icons/visibility_off.svg',
+                            ),
+                            onPressed: () {
+                              isObscure.value = !isObscure.value;
+                            },
                           ),
                         ),
                         onChanged: (value) {
-                          pass.value = value.isNotEmpty;
-                          print('value: $value, pass: ${pass.value}');
+                          isPass.value = value.isNotEmpty;
                         },
                         onSaved: (value) => () {
                           print('$value');
                         },
                       ),
                       Padding(
-                        padding: EdgeInsets.only(top: 20.w, bottom: 15.w),
+                        padding: EdgeInsets.only(top: 20.w),
                         child: OutlinedButton(
                           style: ButtonStyle(
                             side: MaterialStateProperty.all<BorderSide>(
                               BorderSide(
                                 width: 2,
-                                color: (!mail.value || !pass.value)
+                                color: (!isMail.value || !isPass.value)
                                     ? const Color(0xFFB3B3B3)
                                     : Colors.black,
                               ),
@@ -119,17 +147,17 @@ class LoginScreen extends HookWidget {
                               Size(240.w, 54.w),
                             ),
                             foregroundColor: MaterialStateProperty.all<Color>(
-                              ((!mail.value || !pass.value))
+                              ((!isMail.value || !isPass.value))
                                   ? Colors.white
                                   : Colors.black,
                             ),
                             backgroundColor: MaterialStateProperty.all<Color>(
-                              ((!mail.value || !pass.value))
+                              ((!isMail.value || !isPass.value))
                                   ? const Color(0xFFB3B3B3)
                                   : Colors.white,
                             ),
                           ),
-                          onPressed: ((!mail.value || !pass.value))
+                          onPressed: ((!isMail.value || !isPass.value))
                               ? null
                               : () => formKey.currentState!.save(),
                           child: Text(
@@ -140,6 +168,84 @@ class LoginScreen extends HookWidget {
                       ),
                     ],
                   ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5.w, bottom: 30.w),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'パスワードを忘れた方',
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20.w),
+                child: FilledButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all<Size>(
+                      Size(240.w, 54.w),
+                    ),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  child: Text(
+                    '無料で新規登録',
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 15.w, bottom: 25.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        '運営会社',
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        '利用規約',
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'プライバシーポリシー',
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
